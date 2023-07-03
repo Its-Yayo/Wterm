b5f7cb8ac16f08f40a8a56afedf22f6f052627a7
Its-Yayo <a01754574@itesm.mx>
2023-07-03 10:37:22 -0600

    Add improvements and fixes #1

---
+++ src/wterm.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import sys
+import argparse
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+
+current_version = "v0.0.1"
+
+class Wterm:
+    def __init__(self) -> None:
+        print(f"[x] Starting Wterm {current_version}")
+    
+    def reversible(self) -> list:
+        """ Calculates the isoterm reversible process 
+        Args:
+
+        Returns:
+        
+        """
+        pass
+
+    def irreversible(self) -> list:
+        """ Calculates the isoterm irreversible process
+        Args:
+
+        Returns:
+        
+        """
+        pass
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Wterm")
+
+if __name__ == '__main__':
+    main()
